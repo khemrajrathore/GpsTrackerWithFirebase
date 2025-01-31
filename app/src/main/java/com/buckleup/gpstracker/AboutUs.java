cce98b88b20ec5d9423faeebd3108ff5b593af82
KhemrajSingh Rathore <khemraj.rathore@privado.ai>
2025-01-31 10:27:03 +0530

    Update AboutUs.java

--- app/src/main/java/com/buckleup/gpstracker/AboutUs.java
+++ app/src/main/java/com/buckleup/gpstracker/AboutUs.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AboutUs extends AppCompatActivity {        
+public class AboutUs extends AppCompatActivity {          
  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
